66b88ba1de3c2c1b7bd3d9e78cd2ee2b987876bb
Moustapha Ndiaye <moustapha.ndiaye@aecicharterhs.org>
2017-09-30 19:23:22 +0000

    todo show page

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,16 +1,31 @@
 class TodosController < ApplicationController 
     def index
+        @code_word= "#{params[:code]} #{params[:word]}"
+       
     end
     
     def show
-        @todo = {
-            id:params[:id],
+        id= params[:id]
+        if id == "1"
+            @todo = {
+            id:id,
             name: "Wake up at 8am",
             description: "Mon to Fri",
             duration: 15
-        }
-       
-            
+            }
         
+        elsif id == "2" 
+        
+                @todo= {
+                    id:id,
+                    name: "Do homework",
+                    image: "https://daks2k3a4ib2z.cloudfront.net/57510aa8aff2cb202fe7b06b/58585132342b7a0755832815_breakfast.jpg",
+                    duration: 60
+                }
+        else
+            @todo={}
+               
+        end
     end
+        
 end
