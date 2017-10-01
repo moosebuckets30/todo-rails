2c708bf2d99fd1007e86100849525f8d700424f8
Moustapha Ndiaye <moustapha.ndiaye@aecicharterhs.org>
2017-10-01 18:43:12 +0000

    showpage

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -18,5 +18,16 @@ class TodosController < ApplicationController
                
         @todo=Todo.find(params[:id])
     end
+    def update
+        todo= Todo.find(params[:id])
+        todo.update(
+            name: params[:name],
+            description: params[:description],
+            duration: params[:duration],
+            complete: params[:complete]
+        )
+        
+         redirect_to "/todos/#{todo.id}"   
+    end
 end
         
